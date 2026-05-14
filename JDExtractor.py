4b5dc95f7ca5ad92ad80feda2dafa88eb21ccf0d
rohitsen0604-source <rohitsen231133@acropolis.in>
2026-05-14 10:15:39 +0530

    1

--- JDExtractor.py
+++ JDExtractor.py
@@ -1,37 +1,36 @@
-import re
-
 from SkillsExtractor import extract_skills
 
+from ExperienceExtractor import (
+    extract_experience
+)
 
-# -----------------------------------
-# Extract Experience From JD
-# -----------------------------------
-
-def extract_jd_experience(text):
-
-    pattern = r'(\d+)\s*(?:year|years|yr|yrs)'
-
-    match = re.search(pattern, text.lower())
-
-    if match:
-        return int(match.group(1))
-
-    return 0
-
-
-# -----------------------------------
-# Build JD JSON
-# -----------------------------------
 
 def build_jd_json(text):
 
-    skills = extract_skills(text)
+    technical, soft, tools = (
+        extract_skills(text)
+    )
 
-    required_experience = (
-        extract_jd_experience(text)
+    experience = (
+        extract_experience(text)
+    )
+
+    all_skills = list(
+
+        set(
+            technical + soft + tools
+        )
     )
 
     return {
-        "skills": skills,
-        "required_experience": required_experience
+
+        "skills": all_skills,
+
+        "required_experience": (
+
+            experience.get(
+                "total_experience_years",
+                0
+            )
+        )
     }
